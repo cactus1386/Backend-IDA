b70dcfd4c848a65b63b0daacf36673415b78ae21
cactus1386 <cactusinjast@gmail.com>
2024-07-19 16:05:11 -0700

    images

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -1,8 +1,17 @@
 from rest_framework import serializers
-from .models import Product
+from .models import Product, ProductImages
+
+
+class ImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImages
+        fields = ('id', 'image')
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True, source="images_set")
+
     class Meta:
         model = Product
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'description', 'body',
+                  'pic', 'images', 'created_at')
