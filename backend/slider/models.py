5914f8ab651bed939fa74dc514c8da81cc82999a
cactus1386 <cactusinjast@gmail.com>
2024-07-27 14:25:50 -0700

    slider image bug

--- backend/slider/models.py
+++ backend/slider/models.py
@@ -6,6 +6,3 @@ from django.db import models
 class Slider(models.Model):
     image = models.ImageField(upload_to='uploads/')
     created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return self.image
